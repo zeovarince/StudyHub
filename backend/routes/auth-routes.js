44e1b70860ef1eb05eaf9c941c17cd53b8244362
AinurRaftuzzaki <zakipheonix@gmail.com>
2026-06-15 11:25:19 +0700

    API CRUD data Tugas DAN  logika database relation

--- backend/routes/auth-routes.js
+++ backend/routes/auth-routes.js
@@ -2,7 +2,18 @@ const express = require('express');
 const router = express.Router();
 
 const authController = require('../controllers/auth-controller');
+const taskController = require('../controllers/task-controller');
+const authMiddleware = require('../middlewares/auth-middleware');
 
 router.post('/register', authController.register);
 router.post('/login', authController.login);
+
+router.post('/tasks', authMiddleware, taskController.createTask);
+router.get('/tasks', authMiddleware, taskController.getAllTasks);
+router.put('/tasks/:id/status', authMiddleware, taskController.updateTaskStatus);
+router.delete('/tasks/:id', authMiddleware, taskController.deleteTask);
+router.post('/tasks/:id/members', authMiddleware, taskController.inviteMember);
+router.get('/tasks/:id/members', authMiddleware, taskController.getTaskMembers);
+router.delete('/tasks/:id/members/:userId', authMiddleware, taskController.removeMember);
+
 module.exports = router;
